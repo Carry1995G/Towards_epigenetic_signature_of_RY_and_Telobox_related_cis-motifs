d8d02f562f96d924d2320d5b62f80ca364a8ce78
Carry1995G <41380710+Carry1995G@users.noreply.github.com>
2022-07-12 12:34:37 +0200

    Update and rename 3_Motif_annotation.sh to Cis-motif_data_preparation.sh

--- Cis-motif_data_preparation.sh
+++ Cis-motif_data_preparation.sh
@@ -1,45 +1,43 @@
 #!/bin/bash
 
-### Motif annotation using fuzznuc (EMBOSS)###
+#### Motif annotation using fuzznuc (EMBOSS) ####
+
 cd /~
+
+#New folders and path shortcuts
 mkdir -p Motif_GFF
+MOTIFGFF=/Motif_GFF
 
 patterns=("AAACCCTA" "[GA][CA]CCTA[GA]" "CATGCA")
 pnames=("Telobox" "Telolike" "RY")
 for p in "${!patterns[@]}"
 do
-fuzznuc ./INPUT.fasta \
+fuzznuc ./GENOME.fasta \
 	-pattern "${patterns[p]}" \
 	-complement \
 	-rformat gff ./Motif_GFF/OUTPUT.gff
 done
 
+#Converting annotated GFF-files to BED and BAM-files#
 
-### Motif coverage calculation ###
-
-cd /~
-
+#New folders and path shortcuts
 mkdir -p Motif_BAM
 mkdir -p Motif_BED
 mkdir -p Extras
-mkdir -p Motif_BIGWIG
-
-#Path shortcuts
-DOWNLOADS=/Downloads
 EXTRAS=/Extras
 BED=/Motif_BED
 BAM=/Motif_BAM
-BLACKLIST=/Blacklists
+BLACKLIST=/Blacklists #if available
 
 #Generate files containing chromosome length (different formats)#
 cd ${Extras}
 pip install pyfaidx
-faidx ${DOWNLOADS}/GENOME-FASTAFILE.fasta -i chromsizes > Chromlen.txt #used for later steps
-faidx ${DOWNLOADS}/GENOME-FASTAFILE.fasta -i chromsizes > Chromlen.bed #used for later steps
+faidx GENOME.fasta -i chromsizes > Chromlen.txt #used for later steps
+faidx GENOME.fasta -i chromsizes > Chromlen.bed #used for later steps
 cp Chromlen.txt Chromlen.chromsizes
 
 #Reformat annotated GFF-files to BAM-files#
-cd ../Motif_GFF
+cd ${MotifGFF}
 for i in *OUTPUT.gff
 do
 sed -i '/^[#]/ d' $i 			#remove fuzznuc comment 
@@ -60,9 +58,14 @@ bedtools bedtobam -i ${BED}/${i%OUTPUT.bed}shuffle.bed \
 samtools index ${BAM}/${i%OUTPUT.bed}shuffle.bam
 done
 
-#Coverage calculation of all regions #
+#### Motif coverage calculation ####
+
+#New folders and path shortcuts
+mkdir -p Motif_BIGWIG
+MOTIFBW=/Motif_BIGWIG
+
 cd ${BAM}
 for i in *.bam
 do
-bamCoverage -b $i -o ../Motif_BIGWIG/${i%.bam}.bw -p 70 --extendReads 200 --ignoreDuplicates -bl ${BLACKLIST}/Contigs*.bed
+bamCoverage -b $i -o $[MOTIFBW}/${i%.bam}.bw -p 70 --extendReads 200 --ignoreDuplicates -bl ${BLACKLIST}/Contigs*.bed
 done
