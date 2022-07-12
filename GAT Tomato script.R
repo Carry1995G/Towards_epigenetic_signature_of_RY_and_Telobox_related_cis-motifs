074e80006276580d005a04f564661af4c98dc152
Carry1995G <41380710+Carry1995G@users.noreply.github.com>
2022-07-12 15:08:50 +0200

    Update GAT Tomato script.R

--- GAT Tomato script.R
+++ GAT Tomato script.R
@@ -1,4 +1,4 @@
-##s- lycopersicum heatmap##
+##s. lycopersicum heatmap##
 
 library(ggplot2)
 library(xlsx)
@@ -8,18 +8,10 @@ library(tidyr)
 library(stringr)
 library(scales)
 
-
-#RYGAT_result_CHR10_nointergenic.txt
-
-
-setwd("Z:/dep_coupland/grp_turck/carina/Tomato/GAT")
+setwd("/Tomato/GAT")
 
 Motifs <- c("RY", "Telobox", "Telolike")
 out <- c("CDS", "mRNA", "primary_transcript","transcript","pseudogene", "tRNA", "rRNA", "lnc_RNA", "miRNA", "snRNA", "snoRNA", "cDNA_match")
-
-
-#m,"_GAT_Analysis.png"for _nointergenic.txt files Chr1-12
-#m,"_GAT_Analysiswithintergenic.png") for m,"GAT_result_CHROM",c,".txt" CHr2-13
 
 for (m in Motifs) {
   GATresults<-data.frame()
@@ -41,22 +33,15 @@ GATresults <- GATresults %>% #filter(annotation != out) %>%
   mutate(annotation=replace(annotation, annotation=="100bpromoter", "100bp promoter"))
 GATresults$annotation <- factor(GATresults$annotation,levels = rev(c("3kb promoter" , "1kb promoter", "100bp promoter","TSS", "5'UTR", "exon", "intron" ,"gene", "3'UTR", "TTS","downprox","intergenic")))
 
-
 ggplot(GATresults, aes(Chromosome, annotation)) +
   geom_tile(aes(fill = l2fold), colour = "white") +
   scale_fill_gradient2(low = "blue",mid="white", high = "red")+
   facet_wrap(~H3K27me3)+
   theme_bw() +
   scale_x_continuous(breaks=c(2,4,6,8,10,12))+
- # xlab=("chromosome")+ ylab=("annotation")+
   ggtitle(paste(m,"motif"))+
   geom_text(aes(label= paste(c(" ","*")[(abs(qvalue) <= .05)+1])))
- 
 
 ggsave (filename=paste(m,"nointerg_GAT_Analysis.png"),path="Z:/dep_coupland/grp_turck/carina/Tomato/Plots", width=6, height=4)
 
 }
-
-  
-  #log2fold
-  #significant
